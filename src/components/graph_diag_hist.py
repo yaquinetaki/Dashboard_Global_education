239ded25c45f46460e8733ebbb3c83f6b19935f1
yaquinetaki <yaquine.taki@edu.esiee.fr>
2025-12-18 14:25:01 +0100

    aucun_changement

--- src/components/graph_diag_hist.py
+++ src/components/graph_diag_hist.py
@@ -10,4 +10,24 @@ from .variables import (
 
 import plotly.express as px
 
+# Fonction pour créer le diagramme : enfants non scolarisés par genre pour un pays donné
 
+def Diagramme_enfants_non_scolarisé(df, nom_pays):
+
+    # On sélectionne les données du pays choisi
+    df_pays = df[df["Entity"] == nom_pays].sort_values("Year")
+
+    # Création du diagramme en barres empilées
+    fig = px.bar(
+        df_pays,
+        x="Year",
+        y=[col_homme, col_femme],
+        title=f"Enfants non scolarisés : {nom_pays} (Filles vs Garçons)",
+        labels={"value": "Nombre d'enfants", "variable": "Genre"},
+        color_discrete_sequence=["lightblue", "lightpink"]
+    )
+    # Renommage des catégories dans la légende
+    new_names = {col_homme: "Garçons", col_femme: "Filles"}
+    fig.for_each_trace(lambda t: t.update(name = new_names[t.name]))
+    
+    return fig
